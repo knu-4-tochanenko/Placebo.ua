1e5e6505f4d77672234a8fefd2b6d671402eda7f
Tai <justataya@gmail.com>
2020-11-15 02:01:11 +0200

    Add basic scrapper & jsoup library

--- scrapper/src/main/java/Main.java
+++ scrapper/src/main/java/Main.java
@@ -1,5 +1,23 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://pigulka.com.ua/lz/tivorel").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(doc.title());
+        Elements newsHeadlines = doc.select(".field--label-inline");
+        for (Element headline : newsHeadlines) {
+//            System.out.println(headline.html() + "\n\n\n");
+            System.out.println("\n\t" + headline.child(0).html() + " - " + headline.child(1).html());
+        }
     }
 }
