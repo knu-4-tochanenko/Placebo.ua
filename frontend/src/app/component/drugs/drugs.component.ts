53a32a83a4e1541c621c8c39fa1a3f0963cc0ae6
Oleh <oleg.mospan2010@gmail.com>
2020-11-25 22:38:16 +0200

    Add basic Angular Client

---
+++ frontend/src/app/component/drugs/drugs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drugs',
+  templateUrl: './drugs.component.html',
+  styleUrls: ['./drugs.component.css']
+})
+export class DrugsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
