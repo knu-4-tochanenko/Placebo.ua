3d3287cf01423661a1a901386c7eeb938be44e3a
Vladislav <itochanenko@gmail.com>
2020-12-07 07:45:41 +0200

    Add service and logic to main page

--- frontend/src/app/service/drug.service.ts
+++ frontend/src/app/service/drug.service.ts
@@ -16,28 +16,23 @@ export class DrugService {
   constructor(private http: HttpClient) {
   }
 
-  getDrugs(
-    page: number = 0,
-    size: number = 15,
-    sortType: boolean = false,
-    ascSort: boolean = true,
-    sortName: boolean = false,
-    ascName: boolean = true,
-    sortPrice: boolean = false,
-    ascPrice = true,
-  ): Observable<any> {
-    let queryString = '?page=' + page + '&size=' + size;
-    if (sortType) {
-      queryString += '&sort=type' + (ascSort ? '' : ',desc');
+  getDrugs(data: any, settings: any): Observable<any> {
+    let queryString = '?page=' + settings['page'] + '&size=' + settings['size'];
+    if (settings['sortType']) {
+      queryString += '&sort=type' + (settings['ascSort'] ? '' : ',desc');
     }
-    if (sortName) {
-      queryString += '&sort=name' + (ascName ? '' : ',desc');
+    if (settings['sortName']) {
+      queryString += '&sort=name' + (settings['ascName'] ? '' : ',desc');
     }
-    if (sortPrice) {
-      queryString += '&sort=price' + (ascPrice ? '' : ',desc');
+    if (settings['sortPrice']) {
+      queryString += '&sort=price' + (settings['ascPrice'] ? '' : ',desc');
     }
 
-    return this.http.get(API + queryString, httpOptions);
+    return this.http.get(API + queryString, data);
+  }
+
+  searchDrugs(data: any): Observable<any> {
+    return this.http.get(API + 'drugs', data);
   }
 
   getTypes(): Observable<any> {
