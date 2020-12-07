6f1e52697d6822e0506d843850f6c86203bda547
Oleh Mospan <oleg.mospan2010@gmail.com>
2020-12-07 05:49:33 +0200

    Add sort items to drugs page

--- frontend/src/app/component/drugs/drugs.component.ts
+++ frontend/src/app/component/drugs/drugs.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Drugs {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-drugs',
   templateUrl: './drugs.component.html',
   styleUrls: ['./drugs.component.css']
 })
+
+
 export class DrugsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  drugs: Drugs[] = [
+    {value: 'antibiotics', viewValue: 'Антибіотики'},
+    {value: 'gomeophaty', viewValue: 'Гомеопатія'},
+    {value: 'vitamins', viewValue: 'Вітаміни'},
+    {value: 'adds', viewValue: 'Добавки'}
+  ];
 
 }
